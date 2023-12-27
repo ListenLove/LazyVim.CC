b823ebb414f94e1c6a607d157d033a203f5d7eff
ListenLove <wenbinchen150@icloud.com>
2023-12-27 02:25:48 +0000

    feat: 添加常用自定义快捷键设置

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,27 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.api.nvim_set_keymap
+-- 复用 opt 参数
+local opt = { noremap = true, silent = true }
+
+-- Terminal相关
+-- 下边打开terminal
+map("n", "<leader>t", ":sp | terminal<CR>", opt)
+-- 侧边边打开terminal
+map("n", "<leader>vt", ":vsp | terminal<CR>", opt)
+-- 关闭当前
+map("n", "<leader>tc", "<C-w>c", opt)
+
+-- 退出
+map("n", "q", ":q<CR>", opt)
+map("n", "qq", ":q!<CR>", opt)
+map("n", "Q", ":qa!<CR>", opt)
+
+-- visual模式下缩进代码
+map("v", "<", "<gv", opt)
+map("v", ">", ">gv", opt)
+-- 上下移动选中文本
+map("v", "J", ":move '>+1<CR>gv-gv", opt)
+map("v", "K", ":move '<-2<CR>gv-gv", opt)
